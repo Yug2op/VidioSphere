e360482b362453c9993f4448cbff9a59da3069bd
Yugank Tripathi <mr.yugank.2000@gmail.com>
2025-02-22 13:44:49 +0530

    add healthcheck controller and route for server status monitoring

---
+++ backend/src/controllers/healthcheck.controller.js
@@ -0,0 +1,28 @@
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+
+    try {
+        return res
+        .json(
+            new ApiResponse(
+                200,
+                {status:"OK"},
+                "Server is running smoothly."
+            )
+        )
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Heatlthcheck failed. Somthing went wrong."
+        )
+    }
+})
+
+export {
+    healthcheck
+    }
