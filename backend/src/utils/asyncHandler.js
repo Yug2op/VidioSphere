c9458e912e49a977e22e60c7a1fb526422182013
Yugank Tripathi <mr.yugank.2000@gmail.com>
2025-03-08 11:25:33 +0530

    fix: enhance asyncHandler to handle MongoDB Duplicate Key Error with specific messages

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,16 +1,23 @@
-
-const asyncHandler = (requestHandler) => (async (req, res, next) => {
+const asyncHandler = (requestHandler) => async (req, res, next) => {
     try {
-        await requestHandler(req, res, next)
+        await requestHandler(req, res, next);
     } catch (error) {
-        res.status(error.code || 500).json({
-            sucess: false,
-            message: error.message
-        })
+
+        // Handle MongoDB Duplicate Key Error (11000)
+        if (error.code === 11000) {
+            const field = Object.keys(error.keyPattern)[0];
+            const message = field === "email" ? "Email already exists." : "Username already exists.";
+            return res.status(400).json({
+                success: false,
+                message: message,
+            });
+        }
+
+        res.status(error.statusCode || 500).json({
+            success: false,
+            message: error.message || "Internal Server Error",
+        });
     }
-})
+};
 
-
-
-
-export { asyncHandler }
+export { asyncHandler };
