b0f1963ad316083175d18814316b20edeadd624f
Yugank Tripathi <mr.yugank.2000@gmail.com>
2025-04-05 10:55:37 +0530

    fix: update comment routes to support fetching comments by video and tweet IDs

--- backend/src/routes/comment.routes.js
+++ backend/src/routes/comment.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {
     addComment,
     deleteComment,
-    getVideoComments,
+    getComments,
     updateComment,
 } from "../controllers/comment.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
@@ -11,7 +11,8 @@ const router = Router();
 
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/:videoId").get(verifyJWT, getVideoComments).post(verifyJWT, addComment);
+router.route("/video/:videoId").get(verifyJWT, getComments).post(verifyJWT, addComment);
+router.route("/tweet/:tweetId").get(verifyJWT, getComments).post(verifyJWT, addComment);
 router.route("/c/:commentId").delete(verifyJWT, deleteComment).patch(verifyJWT, updateComment);
 
 export default router
